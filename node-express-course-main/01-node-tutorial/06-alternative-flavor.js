61d056e1da131b9752a61dce0657bc7af6da3589
anshg03 <99557054+anshg03@users.noreply.github.com>
2023-01-30 19:26:21 +0530

    first commit

---
+++ node-express-course-main/01-node-tutorial/06-alternative-flavor.js
@@ -0,0 +1,9 @@
+module.exports.items = ['item1', 'item2']
+const person = {
+  name: 'bob',
+}
+
+module.exports.singlePerson = person
+
+//op of this is
+// { items: [ 'item1', 'item2' ], singlePerson: { name: 'bob' } }
